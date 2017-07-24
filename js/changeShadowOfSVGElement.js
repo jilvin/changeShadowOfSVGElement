f9b832ba73424b23189271d9f8c73d7248587061
Jilvin <jilvinjacob@gmail.com>
2017-07-24 13:00:32 +0530

    radiusShadow-circle completed

--- js/changeShadowOfSVGElement.js
+++ js/changeShadowOfSVGElement.js
@@ -8,24 +8,33 @@ function changeShadowOfSVGElement(x1, y1, z1, z2, element1Identifier, element2Id
 
   //Get element1Type.
   var element1Type=document.getElementById(element1Identifier).tagName;
+
   //Get element2Type.
   var element2Type=document.getElementById(element2Identifier).tagName;
-  //Check if both elements are of the same type.
 
+  //Check if both elements are of the same type.
   if(element1Type === element2Type)
   {
 
     if(element1Type === "circle")
-    {}
+    {
+
+      //Get radius of object.
+      var radiusObject = document.getElementById(element1Identifier).getAttribute("r");
+
+      //Calculation of Shadow Radius.
+      var radiusShadow = (radiusObject * ((z2+z1)/z1));
+
+      // Radius of Shadow changed here.
+      document.getElementById(element2Identifier).setAttribute("r", radiusShadow);
+
+    }
     else
     {
 
-        console.error("changeShadowOfSVGElement() :: Unsupported type of elements provided. Aborting operation. elementsType: "+element1Type+"; See: ");
+      console.error("changeShadowOfSVGElement() :: Unsupported type of elements provided. Aborting operation. elementsType: "+element1Type+"; Feel free to support the project with new element types. https://github.com/jilvin/changeShadowOfSVGElement");
 
     }
-    //Calculation of Shadow Radius.
-    // Radius of Shadow changed here.
-    document.getElementById(element2Identifier).setAttribute("r", 2000);
 
   }
   else
